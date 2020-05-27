5ae183eb47dcae0a5db1d204ebdd17a9854fcf6a
Iliy <090101ikn@gmail.com>
2020-05-27 11:19:46 +0300

    4.1-4.7 главы

--- 1st day, 1st try.py
+++ 1st day, 1st try.py
@@ -1,2 +1,40 @@
 #First step
 print("hello there")#тестовый комментарий
+#Изучение округления
+z=4/3
+print(z)
+z=4//3
+print(z)
+#z++
+z=5
+z = z+1+z+1
+print(z) #hmmm падазрительна
+#чек символов для переменных
+s_e = 1
+print("s_e", s_e)
+s_e = "fgr"
+print("s_e", s_e)
+set = 2/4
+print("set", set)
+se = 'gth'
+print("se", se)
+#символы в print
+x = "Don't need a backslash"
+print(x)
+x = 'Can\'t get by without a backslash'
+print(x)
+x = "Backslash your \" character!"
+print(x)
+x = 'You can leave the " alone'
+print(x)
+x = """Starting and ending a string with ' "" triple " characters 
+permits embedded newlines, and the use of  and  without backslashes"""
+#Изучение пересечения типов
+a = 5*'sfe'
+b = (1+4j)*4
+import cmath
+import math
+c = cmath.log(3+3j)
+d = cmath.sqrt(-1)
+g = math.cos(4.6)
+print(a, '\n', b, '\t', c, "\n", d, g)
